6b2cbb1afbb78105e0080dd380561aecebd54761
Charlie Greenman <charlie@razroo.com>
2023-10-08 17:42:17 -0400

    Add code for angular-16.2.0 create-workspace.
    [Url: razroo.com/community/angular-16.2.0/create-workspace-recipe/create-workspace]

---
+++ apps/devgen-test-seven/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'devgen-test-seven-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'devgen-test-seven';
+}
